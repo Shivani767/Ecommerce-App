8bd34f8dfe7d3bc7af950139e58eee524deb35d6
Shivani Bhandari <shivanibhandari767@gmail.com>
2024-12-18 00:56:42 +0530

    Delete server directory

--- server/routes/routes.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-const SignupController = require("../Controllers/Signup");
-const LoginController = require("../Controllers/Login");
-
-router.post('/signup',SignupController.signup)
-router.post('/login',LoginController.login)
-
-
-module.exports = router;
